fc0ac8d6b0c2a2bac7806a7c31f5b07017533af7
zhoraveslov <91185861+zhoraveslov@users.noreply.github.com>
2022-08-11 21:30:08 +0200

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,11 +6,10 @@ const ul = document.querySelectorAll('ul');
 var Items = localStorage.getItem('Items') ? JSON.parse(localStorage.getItem('Items')) : [];
 
 localStorage.setItem('Items', JSON.stringify(Items));
-const data = JSON.parse(localStorage.getItem('Items'));
+ var taskText = inputTask.value;
 
-//Stop the form from submitting when a button is pressed
+
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
 
     if (inputTask.value == '') {
         alert("Enter new task");
@@ -20,7 +19,7 @@ form.addEventListener('submit', function(e) {
         Items.push(taskText);
         localStorage.setItem('Items', JSON.stringify(Items));
         JSON.parse(localStorage.getItem('Items'));
-        itemMaker(inputTask.value)
+        itemMaker(taskText.value)
         inputTask.value = ''
 
     }
@@ -28,30 +27,21 @@ form.addEventListener('submit', function(e) {
 
 const itemMaker = taskText => {
     var li = document.createElement('li')
-    li.textContent = inputTask.value;
+    li.textContent = taskText;
 
     ul[0].appendChild(li);
-
 }
 
 
+function showTask() {
+
+    Items.forEach(element => itemMaker(element)
+
+    );
+}
+
+showTask()
 
 
 
-// button.addEventListener('click', function() {
-//     if (inputTask.value == '') {
-//         alert("Enter new task");
-//     } else {
-//         // const li = document.createElement('li');
-//         // li.textContent = inputTask.value;
-//         // ul[0].appendChild(li);
-//         var taskText = inputTask.value;
-//         Items.push(taskText);
-//         localStorage.setItem('Items', JSON.stringify(Items));
-//         // JSON.parse(localStorage.getItem('Items'));
-//         // itemMaker(inputTask.value)
 
-
-
-//     }
-// })
